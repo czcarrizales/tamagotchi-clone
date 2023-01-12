a08c6347f850a119199f01871a06b99ad218afe5
Chazz <czcarrizales@yahoo.com>
2023-01-12 12:21:36 -0800

    create navbar component with links to home, login, and register components

---
+++ src/pages/Navbar.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function Navbar() {
+  
+
+
+
+  return (
+    <div>
+        <ul>
+            <li><a href="/dashboard">Home</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Register</a></li>
+        </ul>
+    </div>
+  );
+}
+
+export default Navbar;
