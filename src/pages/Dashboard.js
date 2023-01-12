0975b435c5f9c8fff62c6c76d63b1c55c7d48e47
Chazz <czcarrizales@yahoo.com>
2023-01-12 10:24:22 -0800

    using axios for authentication and using the information from the token to set a name and personality for a user's pet, and then attaching that pet to the logged in user

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,29 @@
 import { useEffect, useState } from "react";
 import jwtDecode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 function Dashboard() {
   const navigate = useNavigate();
   const [quote, setQuote] = useState("");
   const [tempQuote, setTempQuote] = useState("");
+  const [petName, setPetName] = useState('')
+  const [petPersonality, setPetPersonality] = useState('')
+  const accessToken = localStorage.getItem('token')
+
+  axios.interceptors.request.use(
+    config => {
+        config.headers.authorization = `Bearer ${accessToken}`
+        return config
+    }
+  )
+
+  const authAxios = axios.create({
+    baseURL: 'http://localhost:5000',
+    headers: {
+        Authorization: `Bearer ${accessToken}`
+    }
+  })
 
   async function populateQuote() {
     const req = await fetch("http://localhost:5000/api/quote", {
@@ -67,6 +85,25 @@ function Dashboard() {
     console.log(tempQuote)
   }
 
+  function handlePetNameChange(event) {
+    setPetName(event.target.value)
+    console.log(event.target.value)
+  }
+
+  function handlePetPersonalityChange(event) {
+    setPetPersonality(event.target.value)
+    console.log(event.target.value)
+  }
+
+  function adopt() {
+    console.log('adopted pet!')
+    authAxios.post('/api/pet', {name: petName, personality: petPersonality})
+        .then((res) => {
+            console.log(res)
+        })
+    console.log(accessToken)
+  }
+
   return (
     <div>
       <h1>Your quote: {quote || "YOU DO NOT HAVE A GOT DANG QUOTE!!!"}</h1>
@@ -80,6 +117,15 @@ function Dashboard() {
         ></input>
         <input type="submit" value="update quote"></input>
       </form>
+      <br/>
+      <h2>Generate a random pet and adopt it!</h2>
+      <input type="text" placeholder="pet name" name="name" value={petName} onChange={handlePetNameChange}></input>
+      <select onChange={handlePetPersonalityChange} value={petPersonality}>
+        <option value="brave">Brave</option>
+        <option value="intelligent">Intelligent</option>
+        <option value="mischevious">Mischevious</option>
+      </select>
+      <button onClick={adopt}>Adopt!</button>
     </div>
   );
 }
