146cee096787c64446e218a89caa1213633f8c01
Chazz <czcarrizales@yahoo.com>
2023-01-12 12:54:42 -0800

    create adoption component to view adoptable pets

---
+++ src/pages/Adopt.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function Adopt() {
+  
+
+
+
+  return (
+    <div>
+        <h1>You can view adoptable pets here.</h1>
+    </div>
+  );
+}
+
+export default Adopt;
