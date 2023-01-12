d254d1885681e0f782bdcc52ac23d270ad6a77a3
Chazz <czcarrizales@yahoo.com>
2023-01-12 13:55:12 -0800

    allowing the get request to view the token and extract the pet data based on the pet id

--- src/pages/ViewPet.js
+++ src/pages/ViewPet.js
@@ -1,13 +1,50 @@
 import { useEffect, useState } from "react";
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 function ViewPet() {
-  
+
+    const token = localStorage.getItem('token')
+    const decodedToken = jwtDecode(token)
+    const petId = decodedToken.pet
+    const [petData, setPetData] = useState()
+
+    const authAxios = axios.create({
+        baseUrl: 'http://localhost:5000',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    useEffect(() => {
+        authAxios.get(`http://localhost:5000/api/pet`)
+        .then((req, res) => {
+            console.log('got pet!')
+            console.log(req.data.pet, 'request')
+            setPetData(req.data.pet)
+            console.log(res)
+        })
+    }, [])
 
 
+    console.log(token)
+    console.log(decodedToken)
+    console.log(petData)
+
+    
 
   return (
     <div>
         <h1>Viewing Pet</h1>
+        {petData && <div>
+            {petData.name}
+            <br/>
+            Personality: {petData.personality}
+            <br/>
+            Happiness: {petData.happiness}
+            <br/>
+            Up for adoption? {String(petData.adoptable)}
+            </div>}
     </div>
   );
 }
