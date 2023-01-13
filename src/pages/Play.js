bba2152cb10647dd1479dc3fa35da19e3ce54957
Chazz <czcarrizales@yahoo.com>
2023-01-12 18:11:22 -0800

    create Play component that allows user to raise their pet's happiness with a button click

---
+++ src/pages/Play.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+
+function Play() {
+
+    const token = localStorage.getItem('token')
+    const decodedToken = jwtDecode(token)
+    const [petData, setPetData] = useState()
+
+    const authAxios = axios.create({
+        baseUrl: 'http://localhost:5000',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    useEffect(() => {
+        authAxios.get(`http://localhost:5000/api/pet`)
+        .then((req, res) => {
+            console.log('got pet!')
+            console.log(req.data.pet, 'request')
+            setPetData(req.data.pet)
+            console.log(res)
+            console.log(petData, 'pet data!')
+        })
+    }, [])
+
+    function raiseHappiness() {
+        authAxios.put('http://localhost:5000/raise-happiness', {_id: petData._id})
+            .then(console.log('raised happiness!'))
+        console.log('happiness raised!')
+    }
+
+  return (
+    <div>
+        <h1>Play with your pet here!</h1>
+        <button onClick={raiseHappiness}>Click to raise happiness!</button>
+    </div>
+  );
+}
+
+export default Play;
