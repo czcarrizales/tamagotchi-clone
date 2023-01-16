47287524f690a3185434224bd3a0c2d0f707259a
Chazz <czcarrizales@yahoo.com>
2023-01-15 16:13:47 -0800

    change display of login, register, and logout nav links depending on if user is logged in or not

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,8 +1,38 @@
 import { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
 
 function Navbar() {
   
+  const token = localStorage.getItem('token')
 
+  const [userData, setUserData] = useState(null)
+
+  console.log(jwtDecode(token))
+  console.log(Date.now())
+
+  useEffect(() => {
+
+    console.log('user data updated')
+  }, [])
+
+  function isLoggedIn() {
+    if (jwtDecode(token).exp * 1000 < Date.now()) {
+      return (
+        <div>
+        <li><a href="/login">Login</a></li>
+            <li><a href="/register">Register</a></li>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <li><a href="#">Logout</a></li>
+        </div>
+      )
+    }
+  }
+
+  
 
 
   return (
@@ -12,9 +42,7 @@ function Navbar() {
             <li><a href="/play">Play</a></li>
             <li><a href="/view-pet">Pet</a></li>
             <li><a href="/adopt">Adopt</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/register">Register</a></li>
-            <li><a href="#">Logout</a></li>
+            {isLoggedIn()}
         </ul>
     </div>
   );
