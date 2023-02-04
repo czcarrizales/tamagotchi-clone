aefdafd2b7c805a655d054ec96f7cdeee09ed183
Chazz <czcarrizales@yahoo.com>
2023-02-04 12:42:03 -0800

    allowing users to play with pet in seperate Play component

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,44 +1,48 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
-import jwtDecode from 'jwt-decode'
+import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 function Play() {
+  const token = localStorage.getItem("token");
+  const decodedToken = jwtDecode(token);
+  const [petData, setPetData] = useState();
 
-    const token = localStorage.getItem('token')
-    const decodedToken = jwtDecode(token)
-    const [petData, setPetData] = useState()
+  const authAxios = axios.create({
+    baseUrl: "http://localhost:5000",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    const authAxios = axios.create({
-        baseUrl: 'http://localhost:5000',
-        headers: {
-            Authorization: `Bearer ${token}`
+  useEffect(() => {
+    authAxios.get(`http://localhost:5000/api/pet`).then((req, res) => {
+      console.log("got pet!");
+      console.log(req.data.pet, "request");
+      setPetData(req.data.pet);
+      console.log(res);
+      console.log(petData, "pet data!");
+    });
+  }, []);
+
+  async function raiseHappiness() {
+    await authAxios
+      .put("http://localhost:5000/raise-happiness", { _id: petData._id })
+      .then((req, res) => {
+        try {
+            console.log('raised!')
+        } catch {
+            console.log('something went wrong')
         }
-    })
-
-    useEffect(() => {
-        authAxios.get(`http://localhost:5000/api/pet`)
-        .then((req, res) => {
-            console.log('got pet!')
-            console.log(req.data.pet, 'request')
-            setPetData(req.data.pet)
-            console.log(res)
-            console.log(petData, 'pet data!')
-        })
-    }, [])
-
-    function raiseHappiness() {
-        authAxios.put('http://localhost:5000/raise-happiness', {_id: petData._id})
-            .then(console.log('raised happiness!'))
-        console.log('happiness raised!')
-    }
+      });
+  }
 
   return (
     <div>
-        <h1>Play with your pet here!</h1>
-        {petData && <div>
-            Your pet's happiness is currently at {petData.happiness}
-            </div>}
-        <button onClick={raiseHappiness}>Click to raise happiness!</button>
+      <h1>Play with your pet here!</h1>
+      {petData && (
+        <div>Your pet's happiness is currently at {petData.happiness}</div>
+      )}
+      <button onClick={raiseHappiness}>Click to raise happiness!</button>
     </div>
   );
 }
