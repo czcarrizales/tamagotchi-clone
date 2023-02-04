b614efb66d7e9688eeebe38c2eb6a72ee1c1ee8d
Chazz <czcarrizales@yahoo.com>
2023-02-04 12:37:20 -0800

    confirmation window, and then navigation to ViewPet page after adopting a pet

--- src/pages/Adopt.js
+++ src/pages/Adopt.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from 'axios'
 import '../styles/Adopt.css'
+import { useNavigate } from "react-router-dom";
 
 function Adopt() {
 
   const token = localStorage.getItem('token')
+  const navigate = useNavigate()
   const [adoptablePets, setAdoptablePets] = useState([])
   
   const authAxios = axios.create({
@@ -19,36 +21,41 @@ useEffect(() => {
     authAxios.get(`http://localhost:5000/api/adoptable-pets`)
     .then((req, res) => {
         req.data.map(pet => {
-          console.log(pet.name)
           setAdoptablePets(oldPets => [...oldPets, pet])
-          console.log(adoptablePets)
         })
         
     })
 }, [])
 
-function adoptPet(petId) {
-  authAxios.put(`http://localhost:5000/adopt-pet`, {_id: petId})
+function adoptPet(pet) {
+  const confirmation = window.confirm(`Are you sure you want to adopt ${pet.name}?`)
+
+  if (confirmation) {
+    authAxios.put(`http://localhost:5000/adopt-pet`, {_id: pet._id})
     .then((req, res) => {
         console.log(req, res)
     })
     .catch((err) => {
       console.log(err)
     })
-  console.log(petId)
+    navigateToPet()
+  }
+}
+
+function navigateToPet() {
+  navigate('/view-pet')
 }
 
 
   return (
     <div className="adopt-component-container">
-        <h1>You can view adoptable pets here.</h1>
+        <h1>adoptable pets</h1>
         {adoptablePets && <div className="all-adoptable-pets-container">
           {adoptablePets.map(pet => {
             return (<div className="adoptable-pet" id={pet._id} key={pet._id}>
               <img src={pet.imageSrc}></img>
               <h2>{pet.name}</h2>
-              <p>Personality: {pet.personality}</p>
-              <button onClick={() => adoptPet(pet._id)}>Adopt!</button>
+              <button onClick={() => adoptPet(pet)}>Adopt!</button>
               </div>)
           })}
           </div>}
