ac931f5b3142c3de8654dcf4bbf4e2951daf63a8
Chazz <czcarrizales@yahoo.com>
2023-02-04 12:41:35 -0800

    adding functions to raise hunger, raise happiness, and put pet up for adoption from ViewPet component

--- src/pages/ViewPet.js
+++ src/pages/ViewPet.js
@@ -1,69 +1,91 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
-import jwtDecode from 'jwt-decode'
-import '../styles/ViewPet.css'
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import "../styles/ViewPet.css";
+import { useNavigate } from "react-router-dom";
 
 function ViewPet() {
+  const token = localStorage.getItem("token");
+  const decodedToken = jwtDecode(token);
+  const [petData, setPetData] = useState();
+  const navigate = useNavigate();
 
-    const token = localStorage.getItem('token')
-    const decodedToken = jwtDecode(token)
-    const [petData, setPetData] = useState()
+  const authAxios = axios.create({
+    baseUrl: "http://localhost:5000",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    const authAxios = axios.create({
-        baseUrl: 'http://localhost:5000',
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
+  useEffect(() => {
+    authAxios.get(`http://localhost:5000/api/pet`).then((req, res) => {
+      setPetData(req.data.pet);
+    });
+  }, []);
 
-    useEffect(() => {
-        authAxios.get(`http://localhost:5000/api/pet`)
-        .then((req, res) => {
-            console.log('got pet!')
-            console.log(req.data.pet, 'request')
-            setPetData(req.data.pet)
-            console.log(res)
-        })
-    }, [])
+  function putUpForAdoption() {
+    const confirmation = window.confirm(`Are you sure you want to put ${petData.name} up for adoption?`)
 
-    function putUpForAdoption() {
-        authAxios.put('http://localhost:5000/api/pet')
-            .then((req, res) => {
-                console.log(req)
-            })
-        console.log('put up your pet for adoption!')
+    if (confirmation) {
+      authAxios.put("http://localhost:5000/api/pet").then((req, res) => {
+        console.log(req);
+      });
+      console.log("put up your pet for adoption!");
+      navigateToAdoptPage()
     }
 
-    function raiseHunger() {
-        authAxios.put('http://localhost:5000/raise-hunger', {_id: petData._id})
-            .then((req, res) => {
-                console.log(res)
-                console.log('hunger raised!')
-            })
-        console.log('hunger raised!')
-    }
+    
+  }
+
+  function raiseHunger() {
+    authAxios
+      .put("http://localhost:5000/raise-hunger", { _id: petData._id })
+      .then((req, res) => {
+        console.log(res);
+        console.log("hunger raised!");
+      });
+    console.log("hunger raised!");
+  }
+
+  function raiseHappiness() {
+    authAxios
+      .put("http://localhost:5000/raise-happiness", { _id: petData._id })
+      .then((req, res) => {
+        console.log(req)
+        console.log(res)
+        console.log('raised happiness')
+      })
+  }
+
+  function navigateToAdoptPage() {
+    navigate('/adopt')
+  }
 
   return (
     <div className="view-pet-container">
-        {petData && <div>
-            <h1>{petData.name}</h1>
-            <img src={petData.imageSrc}></img>
-            <div className="pet-stats-container">
-                <div className="pet-stat">
-                Happiness: {petData.happiness}
-                </div>
-                <div className="pet-stat">
-                Hunger: {petData.hunger}
-                </div>
+      {petData && (
+        <div className="view-pet-inner-container">
+          <h1>{petData.name}</h1>
+          <img src={petData.imageSrc}></img>
+          <div className="pet-stats-container">
+            <div className="pet-stat">
+              <span className="pet-stat-heart">❤</span>
+              {petData.happiness}
             </div>
-            <br/>
-            <button onClick={putUpForAdoption}>Put Up For Adoption</button>
-            <br/>
-            <button onClick={raiseHunger}>Feed</button>
+            <div className="pet-stat">
+              <span>🍭</span>
+              {petData.hunger}
             </div>
-            
-            }
-        {petData == undefined && <div>You have no pets! Go adopt one!</div>}
+          </div>
+          <br />
+          <div className="view-pet-buttons-container">
+            <button onClick={raiseHappiness}>play!</button>
+            <button onClick={raiseHunger}>feed</button>
+            <button onClick={putUpForAdoption}>put up for adoption</button>
+          </div>
+        </div>
+      )}
+      {petData == undefined && <div>You have no pets! Go adopt one!</div>}
     </div>
   );
 }
