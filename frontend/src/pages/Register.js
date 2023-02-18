ab7cd7811d18f25048c33520a4a16b87333260e7
Chazz <czcarrizales@yahoo.com>
2023-02-17 22:30:15 -0800

    adding validation checks to register component

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { useNavigate, Navigate } from 'react-router-dom'
 import '../styles/Register.css'
 import jwtDecode from 'jwt-decode'
@@ -12,8 +12,23 @@ function Register() {
     email: '',
     password: ''
   })
+  const [validPassword, setValidPassword] = useState(false)
+  const [validUsername, setValidUsername] = useState(false)
 
   const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    if (registerState.password.length <= 5) {
+      setValidPassword(false)
+    } else {
+      setValidPassword(true)
+    }
+    if (registerState.name.length <= 5) {
+      setValidUsername(false)
+    } else {
+      setValidUsername(true)
+    }
+  }, [registerState])
 
   // if(jwtDecode(token).exp * 1000 > Date.now()) {
   //   return <Navigate to="/view-pet" />
@@ -24,24 +39,27 @@ function Register() {
 
   async function registerUser(event) {
     event.preventDefault()
-    const response = await fetch('http://localhost:5000/api/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: registerState.name,
-        email: registerState.email,
-        password: registerState.password
+    if (isValid()) {
+      const response = await fetch('http://localhost:5000/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: registerState.name,
+          email: registerState.email,
+          password: registerState.password
+        })
       })
-    })
-
-    const data = await response.json()
-
-    if (data.status === 'ok') {
-        navigate('/login')
+  
+      const data = await response.json()
+  
+      if (data.status === 'ok') {
+          navigate('/login')
+      }
+      console.log(data)
     }
-    console.log(data)
+    
   }
 
   function handleChange(event) {
@@ -50,13 +68,22 @@ function Register() {
     console.log(registerState)
   }
 
+  function isValid() {
+    if (registerState.password.length <= 5 || registerState.name.length <= 5) {
+      return false
+    }
+    return true
+  }
+
   return (
     <div className='register-container'>
       <h1>Register</h1>
       <form onSubmit={registerUser}>
         <input type="text" name="name" placeholder="name" value={registerState.name} onChange={handleChange}></input>
+        {!validUsername && <span>Your username must be at least 6 characters.</span>}
         <input type="email" name="email" placeholder="email" value={registerState.email} onChange={handleChange}></input>
         <input type="password" name="password" placeholder="password" value={registerState.password} onChange={handleChange}></input>
+        {!validPassword && <span>Your password must be at least 6 characters.</span>}
         <input type="submit" value="register"></input>
       </form>
     </div>
