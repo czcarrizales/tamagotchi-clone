6df505d1ad639143ad0218031fcc6ad417b99f03
czcarrizales <czcarrizales@yahoo.com>
2023-02-20 16:57:26 -0800

    attempting to bypass CORS

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://user:user@cluster0.4votkmf.mongodb.net/tamagotchi-clone')
