48670c8ac4e8d1eca01cee557ce2e476c9385a35
czcarrizales <czcarrizales@yahoo.com>
2023-02-21 14:11:39 -0800

    adding pet images to select options

--- frontend/src/pages/CreatePet.js
+++ frontend/src/pages/CreatePet.js
@@ -9,7 +9,7 @@ function CreatePet({userData, handleDataChange, getUserData}) {
   const [quote, setQuote] = useState("");
   const [userHasPet, setUserHasPet] = useState(false)
   const [tempQuote, setTempQuote] = useState("");
-  const [petName, setPetName] = useState("");
+  const [petName, setPetName] = useState("Abigail");
   const [petPersonality, setPetPersonality] = useState("calm");
   const [petImage, setPetImage] = useState("");
   const accessToken = localStorage.getItem("token");
@@ -88,6 +88,9 @@ function CreatePet({userData, handleDataChange, getUserData}) {
   }
 
   function adopt() {
+    if (petImage === '') {
+      return
+    }
     const confirmation = window.confirm('Are you sure you want to create?')
 
     if (confirmation) {
@@ -125,50 +128,108 @@ function CreatePet({userData, handleDataChange, getUserData}) {
       </select>
       <br />
       <h3>Personality</h3>
-      <select onChange={handlePetPersonalityChange} value={petPersonality}>
+      <select onChange={handlePetPersonalityChange} value={petPersonality} className='dashboard-container-personality'>
         <option defaultValue="calm">Calm</option>
         <option value="brave">Brave</option>
         <option value="intelligent">Intelligent</option>
         <option value="mischevious">Mischevious</option>
       </select>
       <br />
+      <h3>Select A Pet </h3>
       <div className="pet-image-select">
-        <h3>Select a pet </h3>
         <div>
-          <input
-            type="radio"
-            value="images/fish.jpg"
-            name="petBody"
-            onChange={handlePetImageChange}
-          ></input>
+         <label>
+         <input
+           type="radio"
+           value="images/dog2.jpg"
+           name="petBody"
+           onChange={handlePetImageChange}
+         ></input>
+           <img src="images/dog2.jpg"></img>
+         </label>
+       </div>
+       <div>
+          
           <label>
-            <img src="images/fish.jpg"></img>
-          </label>
-        </div>
-        <div>
           <input
             type="radio"
             value="images/cat2.jpg"
             name="petBody"
             onChange={handlePetImageChange}
           ></input>
-          <label>
             <img src="images/cat2.jpg"></img>
           </label>
         </div>
         <div>
+          <label>
           <input
             type="radio"
-            value="images/dog2.jpg"
+            value="images/fish.jpg"
             name="petBody"
             onChange={handlePetImageChange}
           ></input>
-          <label>
-            <img src="images/dog2.jpg"></img>
+            <img src="images/fish.jpg"></img>
           </label>
         </div>
+        <div>
+          <label>
+          <input
+            type="radio"
+            value="images/frog.jpg"
+            name="petBody"
+            onChange={handlePetImageChange}
+          ></input>
+            <img src="images/frog.jpg"></img>
+          </label>
+        </div>
+        <div>
+          <label>
+          <input
+            type="radio"
+            value="images/parrot.jpg"
+            name="petBody"
+            onChange={handlePetImageChange}
+          ></input>
+            <img src="images/parrot.jpg"></img>
+          </label>
+        </div>
+        <div>
+          <label>
+          <input
+            type="radio"
+            value="images/lizard.jpg"
+            name="petBody"
+            onChange={handlePetImageChange}
+          ></input>
+            <img src="images/lizard.jpg"></img>
+          </label>
+        </div>
+        <div>
+          <label>
+          <input
+            type="radio"
+            value="images/owl.jpg"
+            name="petBody"
+            onChange={handlePetImageChange}
+          ></input>
+            <img src="images/owl.jpg"></img>
+          </label>
+        </div>
+        <div>
+          <label>
+          <input
+            type="radio"
+            value="images/monkey.jpg"
+            name="petBody"
+            onChange={handlePetImageChange}
+          ></input>
+            <img src="images/monkey.jpg"></img>
+          </label>
+        </div>
+        
+       
       </div>
-      <button onClick={() => {adopt()}}>Adopt!</button>
+      <button className="create-button" onClick={() => {adopt()}}>Create!</button>
     </div>
   );
 }
